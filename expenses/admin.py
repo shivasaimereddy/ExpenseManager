007b78ba0dcd2c01523ea6bf65e8cc10dc658671
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-04 12:51:19 +0530

    Update admin.py

--- expenses/admin.py
+++ expenses/admin.py
@@ -4,7 +4,7 @@ from .models import Expense
 # Register your models here.
 
 class ExpenseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'amount', 'main_category','sub_category','description','mode_of_payment','owner', 'date')
+    list_display = ('id', 'amount', 'main_category','sub_category','description','mode_of_payment','owner', 'date', 'time')
 
 
 admin.site.register(Expense, ExpenseAdmin)
