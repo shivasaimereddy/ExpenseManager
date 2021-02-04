da1e9613ee08ff7ef182d9e480319a32e145d3a8
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-04 17:41:11 +0530

    Update settings.py

--- expensemanager/settings.py
+++ expensemanager/settings.py
@@ -92,19 +92,29 @@ WSGI_APPLICATION = 'expensemanager.wsgi.application'
 
 
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',       
+#        'NAME': 'expense_manager',
+#        'USER': 'root',
+#        'PASSWORD': '',
+#        'HOST': 'localhost',
+#        'PORT': '3306',
+#        'OPTIONS': {
+#            'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'",
+#        }
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'expense_manager',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
 
 
 # Password validation
