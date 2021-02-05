cfe19b7447bf2c334048042d092e46fde259aff1
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-05 19:30:20 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -84,7 +84,7 @@ class ChangePasswordView(generics.UpdateAPIView):
     
     
 @api_view(['GET'])
-#@login_required(login_url='/api/login')
+@login_required(login_url='/api/login')
 @permission_classes([IsAuthenticated])
 def profile(request):
     data = User.objects.filter(username = request.user)
