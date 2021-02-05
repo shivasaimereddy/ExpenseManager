2dcd91c2d4f676c2753635dc4bb1653ee4338b40
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-05 19:29:45 +0530

    Update settings.py

--- expensemanager/settings.py
+++ expensemanager/settings.py
@@ -157,7 +157,7 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'knox.auth.TokenAuthentication',
     ]
