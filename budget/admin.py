c2c17d6f7c0aa8c83c775dc7a24a63702c1b1309
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-06 15:06:28 +0530

    Delete budget directory

--- budget/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Budget
-
-# Register your models here.
-
-class BudgetAdmin(admin.ModelAdmin):
-    list_display = ('total_income', 'total_expense', 'balance', 'owner')
-
-admin.site.register(Budget, BudgetAdmin)
--- budget/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Budget
-
-class BudgetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Budget
-        fields = ['total_income', 'total_expense', 'balance']
