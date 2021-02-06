e044d8c6b3ac5f1ad3270603c1470c1ff1a902af
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-06 15:16:53 +0530

    Updated Total Income

--- income/views.py
+++ income/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView ,ListAPIView
 from .models import Income
 from .serializers import IncomeSerializer
 from rest_framework import permissions
+from django.db.models import Sum
+from rest_framework import viewsets
+from rest_framework.response import Response
+
 
 
 class IncomeList(ListCreateAPIView):
@@ -13,8 +17,16 @@ class IncomeList(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get(self, request):
+        income = Income.objects.filter(owner=request.user)
+        serializer = IncomeSerializer(income, many=True)
+        total_income = income.aggregate(Sum('amount'))['amount__sum']
+        IncomeTable = ({'Total Income': total_income if total_income else 0 , 'Income': serializer.data})
+        return Response(IncomeTable)
+
     def get_queryset(self):
         return Income.objects.filter(owner=self.request.user)
+
 
 
 class IncomeDetailView(RetrieveUpdateDestroyAPIView):
@@ -25,3 +37,7 @@ class IncomeDetailView(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Income.objects.filter(owner=self.request.user)
+
+
+
+
