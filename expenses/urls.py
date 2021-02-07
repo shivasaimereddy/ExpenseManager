50755cc23cc7db337b885ae9d993633a8c8fbc72
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-07 12:47:32 +0530

    Update urls.py

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ExpenseList, ExpenseDetailView
+from .views import ExpenseList, ExpenseDetailView, TotalExpenses
 
 
 urlpatterns = [
     path('', ExpenseList.as_view()),
     path('<int:id>', ExpenseDetailView.as_view()),
+    path('total/', TotalExpenses.as_view()),
+
 ]
