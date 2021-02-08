e96dd67d10b389a73f45ba97008b59dca2797cc4
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 21:00:45 +0530

    Update serializers.py

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Expense
+from .models import Expense, MainCategory, SubCategory, ModeOfPayment
 
 class ExpenseSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +8,19 @@ class ExpenseSerializer(serializers.ModelSerializer):
         fields = ['id', 'amount', 'main_category', 'sub_category', 'description',
                   'mode_of_payment', 'date', 'time'
                   ]
+
+class MainCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MainCategory
+        fields= ['main_category']
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields= ['char','sub_category']
+
+class ModeOfPaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModeOfPayment
+        fields= ['mode_of_payment']
