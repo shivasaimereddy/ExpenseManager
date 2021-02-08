d86755d7bf4cf931c7e3ab260fba2e284e7b5a4a
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 21:02:48 +0530

    Update admin.py

--- income/admin.py
+++ income/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from .models import Income
+from .models import Income, SourceOfIncome, ReceivedSource
 
 # Register your models here.
 
 class IncomeAdmin(admin.ModelAdmin):
     list_display = ('id','amount', 'source_of_income','description','received_source','owner', 'date', 'time')
 
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ['source_of_income']
+
+class ReceivedSourceAdmin(admin.ModelAdmin):
+    list_display = ['received_source']
+
+
 
 admin.site.register(Income, IncomeAdmin)
+admin.site.register(SourceOfIncome, SourceAdmin)
+admin.site.register(ReceivedSource, ReceivedSourceAdmin)
+
 
