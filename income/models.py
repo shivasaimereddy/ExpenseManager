b47cb73e75fc2c2672ca5fe1d84b41538e477601
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 21:03:52 +0530

    Update models.py

--- income/models.py
+++ income/models.py
@@ -3,11 +3,26 @@ from django.utils.timezone import now
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+class SourceOfIncome(models.Model):
+    source_of_income = models.CharField(max_length=226)
+
+    def __str__(self):
+        return self.source_of_income
+
+
+class ReceivedSource(models.Model):
+    received_source = models.CharField(max_length=226)
+
+    def __str__(self):
+        return self.received_source
+
+
 class Income(models.Model):
     amount = models.FloatField()
-    source_of_income = models.CharField(max_length=266)
+    source_of_income = models.ForeignKey(SourceOfIncome, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
-    received_source = models.CharField(max_length=266)
+    received_source = models.ForeignKey(ReceivedSource, on_delete=models.SET_NULL, null=True)
     date = models.DateField(default=now)
     time = models.TimeField(default=now)
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
@@ -18,6 +33,10 @@ class Income(models.Model):
 
     class Meta:
         ordering: ['-date']
+
+
+
+    
 
 
 
