464ac44cc72a78fdd8630a8362877759263859e5
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 20:59:28 +0530

    Update admin.py

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
-from .models import Expense
+from .models import Expense, MainCategory, SubCategory, ModeOfPayment
 
 # Register your models here.
 
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('id', 'amount', 'main_category','sub_category','description','mode_of_payment','owner', 'date', 'time')
 
+class MainCategoryAdmin(admin.ModelAdmin):
+    list_display = ['main_category']
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['char','sub_category']
+
+
+class ModeOfPaymentAdmin(admin.ModelAdmin):
+    list_display = ['mode_of_payment']
 
 admin.site.register(Expense, ExpenseAdmin)
+admin.site.register(MainCategory, MainCategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
+admin.site.register(ModeOfPayment, ModeOfPaymentAdmin)
+
+
