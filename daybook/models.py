35c6f9122fe93b8a1571804c81b695c88ecce022
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 20:58:03 +0530

    Update models.py

--- daybook/models.py
+++ daybook/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Budget(models.Model):
+    Total_Income = models.FloatField()
+    Total_Expenses = models.FloatField()
+    Balance = models.FloatField()
+
+
