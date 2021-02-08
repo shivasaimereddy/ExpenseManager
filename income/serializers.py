60362f0084d0aecfe136b9c4d62dd7a909428672
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 21:05:35 +0530

    Added Source of Income and Received Source

--- income/serializers.py
+++ income/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Income
+from .models import Income, SourceOfIncome, ReceivedSource
 
 class IncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Income
         fields = ['id', 'amount', 'source_of_income', 'description', 'received_source', 'date', 'time']
+
+class SourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SourceOfIncome
+        fields = ['source_of_income']
+
+class ReceivedSourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReceivedSource
+        fields = ['received_source']
+
+
 
