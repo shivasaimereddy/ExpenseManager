ee3e043ccdfc49741a8e54bbec27ab332306944c
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-08 20:57:36 +0530

    Update admin.py

--- daybook/admin.py
+++ daybook/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Budget
 
 # Register your models here.
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('Total_Income', 'Total_Expenses', 'Balance')
+
+admin.site.register(Budget, BudgetAdmin)
+
