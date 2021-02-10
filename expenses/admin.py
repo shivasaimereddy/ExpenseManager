423f37c4b9f56fdbe74d8bacc8af164fa09ca808
Shivasai Mereddy <65664801+shivasaimereddy@users.noreply.github.com>
2021-02-10 11:04:45 +0530

    Update admin.py

--- expenses/admin.py
+++ expenses/admin.py
@@ -6,19 +6,11 @@ from .models import Expense, MainCategory, SubCategory, ModeOfPayment
 class ExpenseAdmin(admin.ModelAdmin):
     list_display = ('id', 'amount', 'main_category','sub_category','description','mode_of_payment','owner', 'date', 'time')
 
-class MainCategoryAdmin(admin.ModelAdmin):
-    list_display = ['main_category']
 
-class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ['char','sub_category']
-
-
-class ModeOfPaymentAdmin(admin.ModelAdmin):
-    list_display = ['mode_of_payment']
 
 admin.site.register(Expense, ExpenseAdmin)
-admin.site.register(MainCategory, MainCategoryAdmin)
-admin.site.register(SubCategory, SubCategoryAdmin)
-admin.site.register(ModeOfPayment, ModeOfPaymentAdmin)
+#admin.site.register(MainCategory, MainCategoryAdmin)
+#admin.site.register(SubCategory, SubCategoryAdmin)
+#admin.site.register(ModeOfPayment, ModeOfPaymentAdmin)
 
 
